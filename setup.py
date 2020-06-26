6150f2a63fc45bff1ea0a3da3fc2bebb0080483d
Edward Beard <ejb207@cam.ac.uk>
2020-06-26 08:30:49 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='pv_database',
+    version='',
+    author='Edward Beard',
+    author_email='ejb207@cam.ac.uk',
+    license='',
+    url='',
+    packages=find_packages(),
+    description='',
+    keywords='text-mining mining chemistry cheminformatics nlp html xml science scientific',
+    zip_safe=False,
+    install_requires=[],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+)
